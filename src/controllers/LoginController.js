97d9ec7fa43444f5bd0c2ca065a0e4208715cdb3
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-06-13 15:53:11 -0300

    Corrige lint

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,1 +1,11 @@
+// const services = require('../services/products');
 
+const ProductsController = {
+  searchProducts: async (_req, res) => {
+    // const products = await services.SearchProductsService();
+
+    res.status(200).json([]);
+  },
+};
+
+module.exports = ProductsController;
