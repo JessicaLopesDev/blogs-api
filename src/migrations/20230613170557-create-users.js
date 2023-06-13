94ddaacf507963c806b5760ea5d999c638fdb005
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-06-13 14:31:13 -0300

    Requisito 1 ok

--- src/migrations/20230613170557-create-users.js
+++ src/migrations/20230613170557-create-users.js
@@ -2,34 +2,26 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-     await queryInterface.createTable('users', {
-       id: {
-         allowNull: false,
-         autoIncrement: true,
-         primaryKey: true,
-         type: Sequelize.INTEGER,
-       },
-       display_name: {
-         type: Sequelize.STRING,
-       },
-       email: {
-         type: Sequelize.STRING,
-       },
-       password: {
-         type: Sequelize.STRING,
-       },
-       image: {
-         type: Sequelize.STRING,
-       },
-       createdAt: {
-         allowNull: false,
-         type: Sequelize.DATE,
-       },
-       updatedAt: {
-         allowNull: false,
-         type: Sequelize.DATE,
-       },
-     });
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      display_name: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+    });
   },
 
   down: async (queryInterface, Sequelize) => {
