27f062771db7e52e56f1c882f2a68c1df53b3f85
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-06-14 18:58:06 -0300

    Requisito 10 ok

---
+++ src/models/BlogPost.js
@@ -0,0 +1,48 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define(
+    'BlogPost',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      title: {
+        type: DataTypes.STRING,
+      },
+      content: {
+        type: DataTypes.STRING,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      published: {
+        type: DataTypes.DATE,
+      },
+      updated: {
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      tableName: 'blog_post',
+      timestamps: false,
+      underscored: true
+    },
+  );
+
+  BlogPost.associate = ({ User }) => {
+    BlogPost.belongsTo(User, {
+      foreignKey: 'user_id',
+      as: 'users'
+    });
+  };
+  
+  return BlogPost;
+};
--- src/models/User.js
+++ src/models/User.js
@@ -1,19 +1,31 @@
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
     },
-    displayName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    image: DataTypes.STRING,
-  },
-  {
-    timestamps: false,
-    tableName: 'users',
-    underscored: true,
-  },
+    {
+      timestamps: false,
+      tableName: 'users',
+      underscored: true,
+    },
   );
+
+  User.associate = ({ BlogPost }) => {
+    User.hasMany(BlogPost, {
+      foreignKey: 'user_id',
+      as: 'blog_posts',
+    });
+  };
+
   return User;
 };
